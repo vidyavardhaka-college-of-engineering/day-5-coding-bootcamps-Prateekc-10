a854c6dcc88382cf76091ec3b222f51620eb5ae1
Prateekc10 <78499323+Prateekc-10@users.noreply.github.com>
2021-02-19 11:19:01 +0000

    program_1 of day 3

--- Program_1.c
+++ Program_1.c
@@ -4,10 +4,22 @@
 
 int main()
 {
+  int i,count=0;
 	char str[10];
 	scanf("%s", str);
 
 	//Write your code here
-
+  for(i=0;str[i]!='\0';i++)
+  {
+    if(str[i]=='a'||
+    str[i]=='e'||
+    str[i]=='i'||
+    str[i]=='o'||
+    str[i]=='u')
+    {
+      count++;
+    }
+  }
+  printf("%d",count);
 	return 0;
 }
